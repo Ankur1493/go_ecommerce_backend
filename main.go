4009901602b655825ea17d214d644df87a7a1dc6
Ankur1493 <ankursharma1493@gmail.com>
2024-07-09 01:26:22 +0530

    controllers created for health and payment

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	http.HandleFunc("/health", getHealhStatus)
+	http.HandleFunc("/payment", createPayment)
+
+	var err error = http.ListenAndServe("localhost:4242", nil)
+	if err != nil {
+		log.Fatal("Sorry the server is down as of now")
+	}
+}
+
+func getHealhStatus(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("health is completely fine")
+}
+
+func createPayment(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Println("payment happen")
+	w.WriteHeader(200)
 }
